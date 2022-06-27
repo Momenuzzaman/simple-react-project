6bb349cc7d5ef4b645a55a823df8c2ffb9066995
mdemon420 <mdeamon4@gmail.com>
2022-06-27 17:25:32 +0600

    set product

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import './Product.css'
 
 const Product = (props) => {
     const { name, price, description, img } = props.product;
     return (
         <div className="product">
             <h1>Name : {name}</h1>
-            <img src={img} alt=''></img>
+            <img className="img" src={img} alt=''></img>
             <h3>Price : {price}</h3>
             <p>Highlight : {description}</p>
         </div>
